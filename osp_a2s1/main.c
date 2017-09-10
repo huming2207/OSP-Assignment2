c48bd9401c1965eb8fe98c38df2522aa890ef103
Jackson Ming Hu <huming2207@gmail.com>
2017-09-10 11:42:29 +1000

    OSP A2s1: implement load/unload functions

--- osp_a2s1/main.c
+++ osp_a2s1/main.c
@@ -1,1 +1,53 @@
 #include "main.h"
+
+static int __init osp_a2s1_init(void)
+{
+	printk(KERN_ALERT "OSP_A2S1: Loading kernel module...\n");
+
+	// Register char device
+	// 0 here means asking OS give me a major device number
+	major_number = register_chrdev(0, CHAR_DEVICE_NAME, &file_ops);
+
+	// major number cannot be less than zero,
+	// It must be wrong if it's -1 (or something else whatever)
+	if(major_number < 0)
+	{
+		printk(KERN_ERR "OSP_A2S1: registering char device failed, kernel returned %d\n", major_number);
+		return major_number;
+	}
+	else
+	{
+		printk(KERN_ALERT "OSP_A2S1: char device registered with major device number: %d\n", major_number);
+	}
+
+	// Register device class
+	osp_device_class = class_create(THIS_MODULE, DEVICE_CLASS_NAME);
+
+	// Detect if something went wrong
+	// IS_ERR() macro detects the pointer is assigned to a valid value
+	if(IS_ERR(osp_device_class))
+	{
+		// If things goes wrong, unregister char device
+		printk(KERN_ERR "OSP_A2S1: Failed to create device class, kernel returned %d\n", PTR_ERROR(osp_device_class));
+		unregister_chardev(major_number, CHAR_DEVICE_NAME);
+		return PTR_ERROR(osp_device_class);
+	}
+	else
+	{
+		printk(KERN_ALERT "OSP_A2S1: device class registered successfully.\n");
+	}
+
+	// Register device, create something at "/dev"
+	osp_device = device_create(osp_device_class, NULL, MKDEV(major_number), NULL, CHAR_DEVICE_NAME);
+
+}
+
+
+static int __exit osp_a2s1_exit(void)
+{
+
+}
+
+// Some macros
+module_init(osp_a2s1_init);
+module_exit(osp_a2s1_exit);
--- osp_a2s1/main.h
+++ osp_a2s1/main.h
@@ -6,6 +6,14 @@
 #include <asm/uaccess.h>
 #include <sys/types.h>
 
+MODULE_AUTHOR("Ming Hu s3554025");
+MODULE_LICENSE("GPL and additional rights");
+MODULE_DESCRIPTION("OSP A2s1 kernel module");
+MODULE_VERSION("1.0");
+
+#define CHAR_DEVICE_NAME "S3554025Device" // Will be "/dev/S3554025Device"
+#define DEVICE_CLASS_NAME "osp_a2" // Will be "/sys/class/osp_a2/S3554025Device"
+
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 static int device_open(struct inode *, struct file *);
@@ -20,8 +28,13 @@ static struct file_operations file_ops =
 };
 
 static int major_number;
-static struct osp_message_container
+struct osp_message_container
 {
-	char message_payload[100] = {'\0'};
+	char message_payload[100];
+	int message_size;
 	struct semaphore message_mutex;
 };
+
+static struct class * osp_device_class;
+static struct device * osp_device;
+static int open_times;
